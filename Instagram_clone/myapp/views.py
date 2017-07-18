ae118b5853f42d83d809cbff227f0ac329ceded7
jatin25297 <55.1jatsharma@gmail.com>
2017-07-18 15:31:37 +0530

    minor changes

--- Instagram_clone/myapp/views.py
+++ Instagram_clone/myapp/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
+from datetime import datetime
 from django.http import HttpResponse
 from django.contrib.auth.hashers import make_password
 
 # Create your views here.
 
 def signup_view(request):
+    today=datetime.now()
 
-    return render(request, 'index.html')
+    return render(request, 'index.html',{'today':today})
